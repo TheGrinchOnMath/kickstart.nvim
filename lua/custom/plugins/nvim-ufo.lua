e8ef3edd6c801b8bc8cf140149a7aec044347857
TheGrinchOnMath <jonathanwakeman@hotmail.com>
2025-03-21 07:11:37 +0100

    working on better understanding and tuning the configs

--- lua/custom/plugins/nvim-ufo.lua
+++ lua/custom/plugins/nvim-ufo.lua
@@ -30,6 +30,8 @@ return {
       vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
       vim.o.foldlevelstart = 99
       vim.o.foldenable = true
+      -- make it look "nice"
+      vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 
       require('ufo').setup()
     end,
